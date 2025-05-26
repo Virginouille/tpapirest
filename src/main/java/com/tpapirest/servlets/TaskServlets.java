d5423d92a54c3b09b94ed819299528fa83b6d779
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-05-26 12:16:27 +0200

    ajout méthode doPost avec erreur

--- src/main/java/com/tpapirest/servlets/TaskServlets.java
+++ src/main/java/com/tpapirest/servlets/TaskServlets.java
@@ -7,6 +7,7 @@ import com.tpapirest.models.Task;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 /***Classe TaskServkets*/
@@ -46,4 +47,39 @@ public class TaskServlets extends HttpServlet {
             }
         }
     }
+
+    /**Méthode pour faire un post*/
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
+        try {
+            // Lire le JSON reçu et le convertir en objet Task
+            Task task = objectMapper.readValue(req.getInputStream(), Task.class);
+
+            if (task.getTitle() == null || task.getTitle().trim().isEmpty()) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.getWriter().write("{\"erreur\":\"Titre obligatoire, ne peut être vide\"}");
+                return;
+            }
+
+            // Initialiser les champs automatiques
+            task.setId(0); // ou laisser à 0 pour auto-incrément
+            task.setDone(false);
+            task.setCreatedAt(LocalDate.from(java.time.LocalDateTime.now()));
+            task.setUpdatedAt (LocalDate.from(java.time.LocalDateTime.now()));
+
+            // Créer la tâche via la DAOTask
+            Task createdTask = taskDAO.create(task);
+
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            objectMapper.writeValue(resp.getOutputStream(), createdTask);
+
+        } catch (Exception e) {
+            // En cas d’erreur JSON mal formée ou autre
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("{\"erreur\":\"format Json Invalid ou erreur de requete\"}");
+        }
+    }
+
 }
